d7294cc4b3b1d83549782c1834af1168d27486ba
Kit Thwaite <kthwaite@penguinrandomhouse.co.uk>
2024-10-09 13:04:04 +0100

    feat(nvim): Update copilot config

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,4 +1,5 @@
-local use_copilot_lua = os.getenv("K6E_COPILOT_LUA") == "1"
+local copilot_lua_env = os.getenv("K6E_COPILOT_LUA")
+local use_copilot_lua = copilot_lua_env ~= nil and copilot_lua_env == "1"
 return {
 	{
 		"github/copilot.vim",
@@ -10,5 +11,25 @@ return {
 		cmd = "Copilot",
 		event = "InsertEnter",
 		enabled = use_copilot_lua,
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					keymap = {
+						accept = false,
+						next = "<C-]>",
+						prev = "<C-[>",
+					},
+				},
+			})
+			vim.keymap.set("i", "<Tab>", function()
+				if require("copilot.suggestion").is_visible() then
+					require("copilot.suggestion").accept()
+				else
+					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+				end
+			end, { desc = "Copilot accept suggestion / Tab" })
+		end,
 	},
 }
